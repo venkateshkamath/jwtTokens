2fb6685bfb04fb31736281b7675f7243d7896db0
venkzy <venkatesh@infomaticscorp.com>
2022-09-12 19:42:41 +0530

    jwt

---
+++ routes/main.js
@@ -0,0 +1,7 @@
+const router = require("express").Router();
+const { login, dashboard } = require("../controllers/main");
+
+router.route("/dashboard").get(dashboard);
+router.route("/login").post(login);
+
+module.exports = router;
